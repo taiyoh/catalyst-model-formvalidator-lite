c4bd6076b5c4b8e105f6972e16859709ae0bf666
Taiyoh Tanaka <sun.basix@gmail.com>
2009-03-25 18:43:02 +0900

    initial commit

---
+++ lib/Catalyst/Model/FormValidator/Lite.pm
@@ -0,0 +1,33 @@
+package Catalyst::Model::FormValidator::Lite;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Catalyst::Model::FormValidator::Lite -
+
+=head1 SYNOPSIS
+
+  use Catalyst::Model::FormValidator::Lite;
+
+=head1 DESCRIPTION
+
+Catalyst::Model::FormValidator::Lite is
+
+=head1 AUTHOR
+
+taiyoh E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
